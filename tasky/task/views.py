58860eac5136df0d6cd4a02c48ef319a9ff3ce62
olatundeawo <awotimilehin.babatunde@gmail.com>
2024-06-15 19:07:45 +0100

    added a view that display and filter the task model

--- tasky/task/views.py
+++ tasky/task/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from .models import Task
 
 # Create your views here.
+def task_list(request):
+    tasks = Task.object.all()
+    return (render(request,'template/task_list.html', {'tasks': tasks}))
+
+
+def fetch_task(request, status):
+    tasks = Task.object.filter(status=status)
+    return (render(request, 'template/fetch_card.html', {'tasks':tasks}))
