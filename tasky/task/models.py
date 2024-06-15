f729fed855e9d9004af6380c502bee681842e0c5
olatundeawo <awotimilehin.babatunde@gmail.com>
2024-06-15 13:08:01 +0100

    created the models

--- tasky/task/models.py
+++ tasky/task/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Task(models.Model):
+    STATUS_CHOICES = [
+        ('not-started', 'Not Started'),
+        ('in-progress', 'In Progress'),
+        ('completed', 'Completed'),
+        ('overdue', 'Overdue'),
+    ]
+
+    PRIORITY_CHOICES = [
+        ('low', 'Low'),
+        ('medium', 'Medium'),
+        ('high', 'High'),
+    ]
+
+    title = models.CharField(max_length=255)
+    description = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateTimeField(blank=True, null=True)
+    status = models.CharField(max_length=12, choices=STATUS_CHOICES, default='not started')
+    priority = models.CharField(max_length=6, choices=PRIORITY_CHOICES, default='medium')
+    assigned_to = models.ForeignKey(User, on_delete=models.SET_NULL, null=True, blank=True, related_name='task')
+
+    def __str__(self):
+        return self.title
+
+        class Meta:
+            ordering = ['due_date', 'created_at']
