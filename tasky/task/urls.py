2e9f905465823ef8469f2d74f159a5279d37c382
olatundeawo <awotimilehin.babatunde@gmail.com>
2024-06-19 16:42:40 +0100

    remove url to the filter_tasks function

--- tasky/task/urls.py
+++ tasky/task/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path, include
 from .views import (task_list, fetch_task, create_task,
- edit_task, delete_task, SearchTask, filter_tasks)
+ edit_task, delete_task, SearchTask)
 
 urlpatterns = ([
     path('', task_list, name='task_list'),
@@ -9,6 +9,6 @@ urlpatterns = ([
     path('edit_task/<int:id>', edit_task, name='edit_task'),
     path('delete_task/<int:id>', delete_task, name='delete_task'),
     path('search/', SearchTask.as_view(), name='search_tasks'),
-    path('filter_tasks/', filter_tasks, name='filter_tasks'),
+    
 ]
 )
